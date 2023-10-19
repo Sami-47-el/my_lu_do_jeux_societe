f9eb5e21c5f729c6d20f35fc6d544a574696e585
Julien-MASSON86 <julien.masson@gmail.com>
2023-10-19 15:16:19 +0200

    ajout loan

--- src/Controller/LoanController.php
+++ src/Controller/LoanController.php
@@ -1,18 +1,51 @@
 <?php
 
 namespace App\Controller;
-
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Category;
+use App\Entity\Loan;
+use App\Entity\Game;
+use App\Form\LoanType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LoanController extends AbstractController
 {
-    #[Route('/loan', name: 'app_loan')]
-    public function index(): Response
+    // #[Route('/loan', name: 'app_loan')]
+    // public function index(EntityManagerInterface $em): Response
+    // {
+    //     $categories = $em->getRepository(Category::class);
+    //     $category = $categories->findAll();
+    //     return $this->render('loan/index.html.twig', [
+    //         'controller_name' => 'LoanController',
+    //         'categories' => $category,
+    //     ]);
+    // }
+
+    #[Route('/loan/{id}', name: 'app_loan')]
+    public function loannig(Request $request, EntityManagerInterface $em, INT $id): Response
     {
+        $loan = new Loan();
+        $form = $this->createForm(LoanType::class, $loan);
+        $form-> handleRequest($request);
+        $games = $em->getRepository(Game::class);
+        $game = $games->find($id);
+        $categories = $em->getRepository(Category::class);
+        $category = $categories->findAll();
+        
+        if ($form->isSubmitted() && $form->isValid()){
+            $loan = $form->getData();
+            $loan->setUser($this->getUser());
+            $loan->setGame($game);
+            $em->persist($loan);
+            $em->flush();
+            return $this->redirectToRoute('app_game'); 
+        }
         return $this->render('loan/index.html.twig', [
-            'controller_name' => 'LoanController',
+            'form' => $form,
+            'categories' => $category,
         ]);
     }
 }
